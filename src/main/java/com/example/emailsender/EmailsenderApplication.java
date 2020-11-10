4813bb7576f208854c0000e13a9ca769d8a4f745
dudziec <dudziecdamian@gmail.com>
2020-11-10 13:33:01 +0100

    fix: update git config

--- src/main/java/com/example/emailsender/EmailsenderApplication.java
+++ src/main/java/com/example/emailsender/EmailsenderApplication.java
@@ -42,22 +42,22 @@ public class EmailsenderApplication implements CommandLineRunner {
 		repository.save(user);
 	}
 
-	@Bean
-	JavaMailSender getJavaMailSender() {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost("smtp.gmail.com");
-		mailSender.setPort(465);
+	// @Bean
+	// JavaMailSender getJavaMailSender() {
+	// 	JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	// 	mailSender.setHost("smtp.gmail.com");
+	// 	mailSender.setPort(465);
 
-		mailSender.setUsername("kwokdamian123@gmail.com");
-		mailSender.setPassword("");
-		Properties properties = mailSender.getJavaMailProperties();
-		properties.put("mail.transport.protocol", "smtp");
-		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.smtp.ssl.enable", "true");
-		properties.put("mail.debug", "true");
+	// 	mailSender.setUsername("kwokdamian123@gmail.com");
+	// 	mailSender.setPassword("");
+	// 	Properties properties = mailSender.getJavaMailProperties();
+	// 	properties.put("mail.transport.protocol", "smtp");
+	// 	properties.put("mail.smtp.auth", "true");
+	// 	properties.put("mail.smtp.ssl.enable", "true");
+	// 	properties.put("mail.debug", "true");
 
-		return mailSender;
-	}
+	// 	return mailSender;
+	// }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
